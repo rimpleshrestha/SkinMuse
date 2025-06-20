9798eb4366dff193928c75071196b3e30cf75a3b
rimpleshrestha <rimpleshrestha133@gmail.com>
2025-06-20 15:29:47 +0545

    commented files

--- lib/views/dashboard_screen.dart
+++ lib/views/dashboard_screen.dart
@@ -1,144 +1,144 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatefulWidget {
-  const DashboardScreen({super.key});
+// class DashboardScreen extends StatefulWidget {
+//   const DashboardScreen({super.key});
 
-  @override
-  State<DashboardScreen> createState() => _DashboardScreenState();
-}
+//   @override
+//   State<DashboardScreen> createState() => _DashboardScreenState();
+// }
 
-class _DashboardScreenState extends State<DashboardScreen> {
-  int _selectedIndex = 0;
+// class _DashboardScreenState extends State<DashboardScreen> {
+//   int _selectedIndex = 0;
 
-  void _onItemTapped(int index) => setState(() => _selectedIndex = index);
+//   void _onItemTapped(int index) => setState(() => _selectedIndex = index);
 
-  @override
-  Widget build(BuildContext context) {
-    const activeColor = Color(0xFFA55166);
+//   @override
+//   Widget build(BuildContext context) {
+//     const activeColor = Color(0xFFA55166);
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFFFFEDF5),
-        title: const Text('SkinMuse'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              'Welcome to SkinMuse!',
-              style: TextStyle(
-                fontSize: 22,
-                fontFamily: 'Inter_Light',
-              ),
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: activeColor,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 16,
-                ),
-              ),
-              onPressed: () {
-                // TODO: Add your quiz navigation logic here
-              },
-              child: const Text(
-                'Take Quiz',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Inter_Bold',
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-        child: Container(
-          height: 70,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 6,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _NavItem(
-                icon: Icons.home_filled,
-                label: 'Home',
-                selected: _selectedIndex == 0,
-                onTap: () => _onItemTapped(0),
-              ),
-              _NavItem(
-                icon: Icons.sticky_note_2_outlined,
-                label: 'Notes',
-                selected: _selectedIndex == 1,
-                onTap: () => _onItemTapped(1),
-              ),
-              _NavItem(
-                icon: Icons.person,
-                label: 'Profile',
-                selected: _selectedIndex == 2,
-                onTap: () => _onItemTapped(2),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Color(0xFFFFEDF5),
+//         title: const Text('SkinMuse'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             const Text(
+//               'Welcome to SkinMuse!',
+//               style: TextStyle(
+//                 fontSize: 22,
+//                 fontFamily: 'Inter_Light',
+//               ),
+//             ),
+//             const SizedBox(height: 24),
+//             ElevatedButton(
+//               style: ElevatedButton.styleFrom(
+//                 backgroundColor: activeColor,
+//                 shape: const RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.all(Radius.circular(20)),
+//                 ),
+//                 padding: const EdgeInsets.symmetric(
+//                   horizontal: 32,
+//                   vertical: 16,
+//                 ),
+//               ),
+//               onPressed: () {
+//                 // TODO: Add your quiz navigation logic here
+//               },
+//               child: const Text(
+//                 'Take Quiz',
+//                 style: TextStyle(
+//                   fontSize: 18,
+//                   fontFamily: 'Inter_Bold',
+//                   color: Colors.white,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       bottomNavigationBar: Padding(
+//         padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+//         child: Container(
+//           height: 70,
+//           decoration: BoxDecoration(
+//             color: Colors.white,
+//             borderRadius: BorderRadius.circular(30),
+//             boxShadow: const [
+//               BoxShadow(
+//                 color: Colors.black26,
+//                 blurRadius: 6,
+//                 offset: Offset(0, 3),
+//               ),
+//             ],
+//           ),
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             children: [
+//               _NavItem(
+//                 icon: Icons.home_filled,
+//                 label: 'Home',
+//                 selected: _selectedIndex == 0,
+//                 onTap: () => _onItemTapped(0),
+//               ),
+//               _NavItem(
+//                 icon: Icons.sticky_note_2_outlined,
+//                 label: 'Notes',
+//                 selected: _selectedIndex == 1,
+//                 onTap: () => _onItemTapped(1),
+//               ),
+//               _NavItem(
+//                 icon: Icons.person,
+//                 label: 'Profile',
+//                 selected: _selectedIndex == 2,
+//                 onTap: () => _onItemTapped(2),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class _NavItem extends StatelessWidget {
-  final IconData icon;
-  final String label;
-  final bool selected;
-  final VoidCallback onTap;
+// class _NavItem extends StatelessWidget {
+//   final IconData icon;
+//   final String label;
+//   final bool selected;
+//   final VoidCallback onTap;
 
-  const _NavItem({
-    required this.icon,
-    required this.label,
-    required this.selected,
-    required this.onTap,
-  });
+//   const _NavItem({
+//     required this.icon,
+//     required this.label,
+//     required this.selected,
+//     required this.onTap,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    const activeColor = Color(0xFFA55166);
-    return Expanded(
-      child: InkWell(
-        borderRadius: BorderRadius.circular(30),
-        onTap: onTap,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, color: selected ? activeColor : Colors.grey),
-            const SizedBox(height: 4),
-            Text(
-              label,
-              style: TextStyle(
-                fontSize: 12,
-                color: selected ? activeColor : Colors.grey,
-                fontWeight: selected ? FontWeight.bold : FontWeight.normal,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     const activeColor = Color(0xFFA55166);
+//     return Expanded(
+//       child: InkWell(
+//         borderRadius: BorderRadius.circular(30),
+//         onTap: onTap,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Icon(icon, color: selected ? activeColor : Colors.grey),
+//             const SizedBox(height: 4),
+//             Text(
+//               label,
+//               style: TextStyle(
+//                 fontSize: 12,
+//                 color: selected ? activeColor : Colors.grey,
+//                 fontWeight: selected ? FontWeight.bold : FontWeight.normal,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
